99a461c0225d08cd8a21b9cf0dacb620b08bbcea
marcoDmc <marcodamasceno07@gmail.com>
2024-01-30 01:29:48 -0300

    feat: added function that handles returning a message on the main route and sending email through nodemailer

---
+++ src/controllers/HandleGet.js
@@ -0,0 +1,7 @@
+module.exports = {
+    async HandleGetHelloWorld(req, res) {
+
+        res.send("welcome to my simple api")
+
+    }
+}
---
+++ src/controllers/HandlePushEmail.js
@@ -0,0 +1,32 @@
+"use strict";
+const nodemailer = require("nodemailer");
+require("dotenv").config()
+
+
+
+module.exports = {
+    async HandlePushEmail(req, res) {
+        const transporter = nodemailer.createTransport({
+            service: "Gmail",
+            host: "smtp.gmail.com",
+            port: 465,
+            secure: true,
+            auth: {
+                user: process.env.EMAIL,
+                pass: process.env.PASSWORD,
+            },
+        });
+        const info = await transporter.sendMail({
+            from: `${req.body.email}`,
+            to: "marcodamasceno0101@outlook.com",
+            subject: req.body.title,
+            text: req.body.content,
+            html: `
+            <b>${req.body.title}</b>
+            <p>${req.body.content}</p>
+            `,
+        });
+
+        console.log("Message sent: %s", info.messageId);
+    }
+}
