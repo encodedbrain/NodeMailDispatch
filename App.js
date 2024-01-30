462bcfd33fdc182d2e139247531146e24ef2d25b
marcoDmc <marcodamasceno07@gmail.com>
2024-01-30 01:27:54 -0300

    chore: Application main file configuration

---
+++ App.js
@@ -0,0 +1,15 @@
+
+require("dotenv").config()
+const express = require("express");
+const app = express()
+const router = require("./src/routes/Routes")
+const cors = require("cors")
+
+app.use(express.json())
+app.use(cors())
+app.use(express.urlencoded({ extended: true }))
+app.use(router)
+
+app.listen(process.env.PORT, () => console.log(`server running in http://localhost:${process.env.PORT}`))
+
+
