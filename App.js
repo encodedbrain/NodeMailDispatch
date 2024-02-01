93f39c6a02b86be6b8fba34cc5c7ffe5d9bb6767
marcoDmc <marcodamasceno07@gmail.com>
2024-02-01 17:46:29 -0300

    chore: config cors

--- App.js
+++ App.js
@@ -6,10 +6,8 @@ const router = require("./src/routes/Routes")
 const cors = require("cors")
 
 app.use(express.json())
-app.use(cors({
-    origin: "http://localhost:5173",
-    credentials: true
-}))
+app.use(cors())
 app.use(express.urlencoded({ extended: true }))
 app.use(router)
 
+app.listen(process.env.PORT, () => console.log(`http://localhost:${process.env.PORT}`))
